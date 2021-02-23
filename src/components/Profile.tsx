02c62975b29bdb9416812f84a832450461317ab3
Beatriz <beatrizsallesss@gmail.com>
2021-02-23 16:34:18 -0300

    🩹 fix : Fix padding problems in progress bars.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,22 +6,22 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-    <div className={styles.profile}>
-      <div className={styles.avatar}>
-      <div className={styles.avatarImage}>
-        <img src="https://user-images.githubusercontent.com/62452619/108862320-e0ca5000-75ce-11eb-8778-a2c1b3e01453.png" alt="Female Avatar" />
+      <div className={styles.profile}>
+        <div className={styles.avatar}>
+          <div className={styles.avatarImage}>
+            <img src="https://user-images.githubusercontent.com/62452619/108862320-e0ca5000-75ce-11eb-8778-a2c1b3e01453.png" alt="Female Avatar" />
+          </div>
+          <div>
+            <p>Level 1</p>
+          </div>
+        </div>
       </div>
-      <div>
-      <p>Lv. 1</p>
+      <div className={styles.infos}>
+        <strong>DarkIceFoxy</strong>
+        <ExperienceBar />
+        <LifeBar />
       </div>
-      </div>
+
     </div>
-    <div className={styles.infos}>
-    <strong>DarkIceFoxy</strong>
-    <ExperienceBar />
-    <LifeBar />
-  </div>
-  
-  </div>
   );
 }

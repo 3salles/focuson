80df79eeb6982e90c54d349a2695b86018023128
Beatriz <beatrizsallesss@gmail.com>
2021-02-22 23:21:07 -0300

    🚧 chore: Create life bar component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ExperienceBar } from './components/ExperienceBar';
+import { LifeBar } from './components/LifeBar';
 import './styles/global.css';
 
 function App() {
   return (
     <div className="container">
+      <>
       <ExperienceBar />
+      <LifeBar />
+      </>
     </div>
   );
 }

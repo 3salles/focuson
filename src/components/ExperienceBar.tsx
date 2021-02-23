29c6520a1f65c276990ae7ee9fd7cd35177aad39
Beatriz <beatrizsallesss@gmail.com>
2021-02-23 14:03:36 -0300

    🚧 chore: Create profile component.

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -2,12 +2,18 @@ import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
   return (
+    <>
     <div className={ styles.experienceBar }>
-      <span>* XP</span>
-      <div>
+      <span>*</span>
+      <div className={styles.experienceProgress}>
         <div style={{ width: '50%'}} />
       </div>
-      <span>300/600</span>
+      
     </div>
+    <div className={styles.status}>
+    <span>XP</span>
+    <span>300/600</span>
+    </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { ExperienceBar } from '../components/ExperienceBar'
 import { LifeBar } from '../components/LifeBar'
+import { Profile } from '../components/Profile'
+
+import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <>
       <ExperienceBar />
       <LifeBar />
       </>
+
+      <section>
+        <div className={styles.profile}><Profile /></div>
+        <div></div>
+      </section>
     </div>
   )
 }

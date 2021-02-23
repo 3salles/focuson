74912b6b968c49cf4403d313d0e2f222b1e078af
Beatriz <beatrizsallesss@gmail.com>
2021-02-23 18:13:31 -0300

    🚧 chore: Add button Countdown.

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,16 +2,21 @@ import styles from '../styles/components/Countdown.module.css'
 
 export function CountDown() {
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>2</span>
+          <span>5</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>0</span>
+          <span>0</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+      <button type="button" className={styles.countdownButton}>
+        Iniciar um ciclo
+      </button>
     </div>
   )
 }

3270e3542f5c0a64728e8463af35f23746a8c592
Beatriz <beatrizsallesss@gmail.com>
2021-02-23 11:43:26 -0300

    🚚 chore: Move to NextJS project.

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from '../components/ExperienceBar'
+import { LifeBar } from '../components/LifeBar'
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <>
+      <ExperienceBar />
+      <LifeBar />
+      </>
+    </div>
+  )
+}

bdf28c6764488dbb9005c50f040b0f62b5b3e489
Beatriz <beatrizsallesss@gmail.com>
2021-02-23 17:59:02 -0300

    🍱 chore: Add favicon and title on site.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import { CompletedChallenges } from '../components/CompletedChallenges'
 import { CountDown } from '../components/Countdown'
 import { Profile } from '../components/Profile'
 
+import Head from 'next/head'
+
 import styles from '../styles/pages/Home.module.css'
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | FocusOn</title>
+      </Head>
+      
       <section>
         <div>
           <Profile />

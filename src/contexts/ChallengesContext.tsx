a8eab5dd2e4a7196e7744f4f59b66f120228ffc4
Beatriz <beatrizsallesss@gmail.com>
2021-02-25 17:50:03 -0300

    ✨ feat: Add Notification for new challenge.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from '../../challenges.json'
 
 
@@ -35,6 +35,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level +1) * 4, 2)
 
+  useEffect(() => {
+    Notification.requestPermission()
+  }, [])
+
 
   function levelUp() {
     setLevel(level + 1)
@@ -45,6 +49,13 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex]
 
     setActiveChallenge(challenge)
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio 🎉', {
+        body: `Valendo ${challenge.amount}xp!`,
+        icon: '../../public/favicon.png'
+      })
+    }
   }
 
   function resetChallenge() {

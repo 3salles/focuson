ffb6c1520364ec2a542741bb7a6f80fd900653a1
Beatriz <beatrizsallesss@gmail.com>
2021-02-25 17:16:35 -0300

    🚧 chore: Create Countdown context.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
+import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
   const { activeChallenge, resetChallenge, completeChallenge } =  useContext(ChallengesContext)
+  const { resetCountdown } = useContext(CountdownContext)
+
+  function handleChallengeSucceeded(){
+    completeChallenge()
+    resetCountdown()
+  }
+
+  function handleChallengeFailed(){
+    resetChallenge()
+    resetCountdown()
+  }
 
   return(
     <div className={styles.challengeBoxContainer}>
@@ -19,14 +31,14 @@ export function ChallengeBox() {
             <button
             type='button'
             className={styles.challengeFailedButton}
-            onClick={resetChallenge}
+            onClick={handleChallengeFailed}
             >
               Falhei</button>
 
             <button
             type='button'
             className={styles.challengeSucceededButton}
-            onClick={completeChallenge}
+            onClick={handleChallengeSucceeded}
             >
               Completei</button>
           </footer>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/global.css'
-import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
       <Component {...pageProps} />
     </ChallengesProvider>
-  ) 
+  )
 }
 
 export default MyApp

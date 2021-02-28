9884386a3e06121a8ae37eab28fa9b67e1ce6e87
Beatriz <beatrizsallesss@gmail.com>
2021-02-28 16:58:57 -0300

    🩹 fix: Countdown reset function wrong time.

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false)
   }
 
